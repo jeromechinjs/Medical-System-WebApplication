33cc69a4ee94df12d2ceb71d08d6d8ce38d03162
ions-js04 <behnc.natural@gmail.com>
2023-08-16 03:45:14 +0800

    signup page completed

--- Medical-System-WebApplication/LoginPage.Master.designer.cs
+++ Medical-System-WebApplication/LoginPage.Master.designer.cs
@@ -76,5 +76,14 @@ namespace Medical_System_WebApplication
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btn_Login;
+
+        /// <summary>
+        /// patientSignUp control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder patientSignUp;
     }
 }
