cd2307fd20c3b97ec83bacf6a58c45a76590ff31
2112531 <2112531@m365.tarc.edu.my>
2023-08-19 23:12:59 +0800

    tmr will design appointment management (bootstrap for grid view is done, only left some minor design)

--- Medical-System-WebApplication/Appointment_Management.aspx.cs
+++ Medical-System-WebApplication/Appointment_Management.aspx.cs
@@ -29,10 +29,12 @@ namespace Medical_System_WebApplication
         void PopulateGridView()
         {
             DataTable table = new DataTable();
-
-            con.Open();
+            
             dataAdapter = new SqlDataAdapter("SELECT * FROM Appointment", con);
+            con.Close();
+
             dataAdapter.Fill(table);
+
 
             if (table.Rows.Count > 0)
             {
@@ -51,23 +53,68 @@ namespace Medical_System_WebApplication
                 gvAppointmentManage.Rows[0].Cells[0].Text = "No Appointment is found !!";
                 gvAppointmentManage.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
             }
+            
 
         }
 
         protected void gvAppointmentManage_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
-            con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-            con.Open();
-            string query = "DELETE FROM Appointment WHERE AppointmentID = @AppointmentID";
-            cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@AppointmentID", Convert.ToInt32(gvAppointmentManage.DataKeys[e.RowIndex].Value.ToString()));
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                con.Open();
+                string query = "DELETE FROM Appointment WHERE AppointmentID = @AppointmentID";
+                cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@AppointmentID", Convert.ToInt32(gvAppointmentManage.DataKeys[e.RowIndex].Value.ToString()));
 
-            cmd.ExecuteNonQuery();
-            PopulateGridView();
-            lblSuccessMessage.Text = "Appointment Checked in !";
-            lblErrorMessage.Text = "";
+                cmd.ExecuteNonQuery();
+                PopulateGridView();
+                lblSuccessMessage.Text = "Appointment Checked in !";
+                lblErrorMessage.Text = "";
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+            
 
         }
+
+        protected void gvAppointmentManage_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {
+                    con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
+                    string query = "INSERT INTO Appointment (PatientID, AppointmentDate, AppointmentTime, ConcernAndRequest, SpecialtyID) VALUES (@PatientID, @AppointmentDate, @AppointmentTime, @ConcernAndRequest, @SpecialtyID)";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@PatientID", (gvAppointmentManage.FooterRow.FindControl("txtPatientIDFooter") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@AppointmentDate", (gvAppointmentManage.FooterRow.FindControl("txtAppointmentDateFooter") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@AppointmentTime", (gvAppointmentManage.FooterRow.FindControl("txtAppointmentTimeFooter") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@ConcernAndRequest", (gvAppointmentManage.FooterRow.FindControl("txtConcernAndRequestFooter") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@SpecialtyID", (gvAppointmentManage.FooterRow.FindControl("txtSpecialtyIDFooter") as TextBox).Text.Trim());
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    PopulateGridView();
+                    lblSuccessMessage.Text = "New Appointment Added!";
+                    lblErrorMessage.Text = "";
+
+                    con.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+            
+        }
+
 
     }
 }
