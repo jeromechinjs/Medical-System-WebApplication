8634803c91e18bc4e854aa3bcb651229978e0fe5
2112531 <2112531@m365.tarc.edu.my>
2023-08-19 19:12:56 +0800

    stuck

---
+++ Medical-System-WebApplication/Appointment_Management.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Medical_System_WebApplication
+{
+    public partial class Appointment_Management : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        SqlCommand cmd;
+        SqlDataAdapter dataAdapter;
+        DataSet ds = new DataSet();
+        string query;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateGridView();
+            }
+        }
+
+        void PopulateGridView()
+        {
+            DataTable table = new DataTable();
+
+            con.Open();
+            dataAdapter = new SqlDataAdapter("SELECT * FROM Appointment", con);
+            dataAdapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                gvAppointmentManage.DataSource = table;
+                gvAppointmentManage.DataBind();
+            }
+            else
+            {
+                table.Rows.Add(table.NewRow());
+
+                gvAppointmentManage.DataSource = table;
+                gvAppointmentManage.DataBind();
+                gvAppointmentManage.Rows[0].Cells.Clear();
+                gvAppointmentManage.Rows[0].Cells.Add(new TableCell());
+                gvAppointmentManage.Rows[0].Cells[0].ColumnSpan = table.Columns.Count;
+                gvAppointmentManage.Rows[0].Cells[0].Text = "No Appointment is found !!";
+                gvAppointmentManage.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+
+        }
+
+        protected void gvAppointmentManage_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            con.Open();
+            string query = "DELETE FROM Appointment WHERE AppointmentID = @AppointmentID";
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@AppointmentID", Convert.ToInt32(gvAppointmentManage.DataKeys[e.RowIndex].Value.ToString()));
+
+            cmd.ExecuteNonQuery();
+            PopulateGridView();
+            lblSuccessMessage.Text = "Appointment Checked in !";
+            lblErrorMessage.Text = "";
+
+        }
+
+    }
+}
