1a89428b56e6683fa9e5a5452c9e07596912b62a
Lee Jaeky <leejaeky30@gmail.com>
2023-08-20 02:42:26 +0800

    Product Done, other than DB picture

---
+++ Medical-System-WebApplication/Cart.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Dashboard.Master" AutoEventWireup="true" CodeBehind="Cart.aspx.cs" Inherits="Medical_System_WebApplication.Cart" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="section1" runat="server">
+
+
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="productName" HeaderText="Item">
+                    <HeaderStyle HorizontalAlign="Center" />
+                </asp:BoundField>
+                <asp:ImageField DataImageUrlField="productImage" HeaderText="Image">
+                    <HeaderStyle HorizontalAlign="Center" />
+                </asp:ImageField>
+                <asp:BoundField DataField="productPrice" HeaderText="Price" />
+                <asp:TemplateField HeaderText="Quantity">
+                    <ItemTemplate>
+                        <input type="number" id="quantity" name="quantity" min="1" value="1">
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="productTotal" HeaderText="Total" />
+                <asp:CommandField DeleteText="Remove" ShowDeleteButton="True" />
+            </Columns>
+        </asp:GridView>
+        <asp:Button ID="Button1" runat="server" Text="CheckOut" />
+        <br />
+    </form>
+
+
+</asp:Content>
--- Medical-System-WebApplication/Products.aspx.cs
+++ Medical-System-WebApplication/Products.aspx.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace Medical_System_WebApplication
 {
@@ -11,7 +17,33 @@ namespace Medical_System_WebApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack || DropDownList2.SelectedValue == "-1")
+            {
+                string command = SqlDataSource1.SelectCommand; // added just for debug purpose
+                SqlDataSource1.SelectCommand = "SELECT * FROM Product";
+                SqlDataSource1.DataBind();
+                DataList1.DataBind();
+            }
+            //if (!Page.IsPostBack)
+            //{
+            //    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
 
+            //    SqlCommand com = new SqlCommand("SELECT * from Product", con);
+            //    SqlDataAdapter da = new SqlDataAdapter(com);
+            //    DataSet ds = new DataSet();
+            //    da.Fill(ds, "Product");
+
+            //    con.Open();
+
+
+
+            //    con.Close();
+            //}
+        }
+
+        protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            Response.Redirect("Cart.aspx?id=" + e.CommandArgument.ToString());
         }
     }
 }
