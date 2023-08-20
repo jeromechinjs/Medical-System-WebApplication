67420434ce6a4cc8c0b849fbfb22f6d60693a287
2112531 <2112531@m365.tarc.edu.my>
2023-08-20 15:19:29 +0800

    waiting

--- Medical-System-WebApplication/Dashboard.Master.cs
+++ Medical-System-WebApplication/Dashboard.Master.cs
@@ -14,61 +14,61 @@ namespace Medical_System_WebApplication
     {
         // Appointment Mangement
 
-        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-        SqlCommand cmd;
-        SqlDataAdapter dataAdapter;
-        DataSet ds = new DataSet();
-        string query;
+        //SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        //SqlCommand cmd;
+        //SqlDataAdapter dataAdapter;
+        //DataSet ds = new DataSet();
+        //string query;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                PopulateGridView();
-            }
+            //if (!IsPostBack)
+            //{
+            //    PopulateGridView();
+            //}
         }
 
-        void PopulateGridView()
-        {
-            DataTable table = new DataTable();
+    //    void PopulateGridView()
+    //    {
+    //        DataTable table = new DataTable();
 
-            con.Open();
-            dataAdapter = new SqlDataAdapter("SELECT * FROM Appointment", con);
-            dataAdapter.Fill(table);
+    //        con.Open();
+    //        dataAdapter = new SqlDataAdapter("SELECT * FROM Appointment", con);
+    //        dataAdapter.Fill(table);
 
-            if (table.Rows.Count > 0)
-            {
-                gvAppointmentManage.DataSource = table;
-                gvAppointmentManage.DataBind();
-            }
-            else
-            {
-                table.Rows.Add(table.NewRow());
+    //        if (table.Rows.Count > 0)
+    //        {
+    //            gvAppointmentManage.DataSource = table;
+    //            gvAppointmentManage.DataBind();
+    //        }
+    //        else
+    //        {
+    //            table.Rows.Add(table.NewRow());
 
-                gvAppointmentManage.DataSource = table;
-                gvAppointmentManage.DataBind();
-                gvAppointmentManage.Rows[0].Cells.Clear();
-                gvAppointmentManage.Rows[0].Cells.Add(new TableCell());
-                gvAppointmentManage.Rows[0].Cells[0].ColumnSpan = table.Columns.Count;
-                gvAppointmentManage.Rows[0].Cells[0].Text = "No Appointment is found !!";
-                gvAppointmentManage.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
-            }
+    //            gvAppointmentManage.DataSource = table;
+    //            gvAppointmentManage.DataBind();
+    //            gvAppointmentManage.Rows[0].Cells.Clear();
+    //            gvAppointmentManage.Rows[0].Cells.Add(new TableCell());
+    //            gvAppointmentManage.Rows[0].Cells[0].ColumnSpan = table.Columns.Count;
+    //            gvAppointmentManage.Rows[0].Cells[0].Text = "No Appointment is found !!";
+    //            gvAppointmentManage.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+    //        }
 
-        }
+    //    }
 
-        protected void gvAppointmentManage_RowDeleting(object sender, GridViewDeleteEventArgs e)
-        {
-            con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-            con.Open();
-            string query = "DELETE FROM Appointment WHERE AppointmentID = @AppointmentID";
-            cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@AppointmentID", Convert.ToInt32(gvAppointmentManage.DataKeys[e.RowIndex].Value.ToString()));
+    //    protected void gvAppointmentManage_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    //    {
+    //        con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+    //        con.Open();
+    //        string query = "DELETE FROM Appointment WHERE AppointmentID = @AppointmentID";
+    //        cmd = new SqlCommand(query, con);
+    //        cmd.Parameters.AddWithValue("@AppointmentID", Convert.ToInt32(gvAppointmentManage.DataKeys[e.RowIndex].Value.ToString()));
 
-            cmd.ExecuteNonQuery();
-            PopulateGridView();
-            lblSuccessMessage.Text = "Appointment Checked in !";
-            lblErrorMessage.Text = "";
+    //        cmd.ExecuteNonQuery();
+    //        PopulateGridView();
+    //        lblSuccessMessage.Text = "Appointment Checked in !";
+    //        lblErrorMessage.Text = "";
 
-        }
+    //    }
     }
 }
--- Medical-System-WebApplication/Dashboard.Master.designer.cs
+++ Medical-System-WebApplication/Dashboard.Master.designer.cs
@@ -24,42 +24,6 @@ namespace Medical_System_WebApplication
         protected global::System.Web.UI.WebControls.ContentPlaceHolder navItemAppointment;
 
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// gvAppointmentManage control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gvAppointmentManage;
-
-        /// <summary>
-        /// lblSuccessMessage control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblSuccessMessage;
-
-        /// <summary>
-        /// lblErrorMessage control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblErrorMessage;
-
-        /// <summary>
         /// upcomingAppointments control.
         /// </summary>
         /// <remarks>
