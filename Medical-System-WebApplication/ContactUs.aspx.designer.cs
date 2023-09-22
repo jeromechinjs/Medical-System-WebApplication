9ab51bc0b4ef9b241adb5ffeed188df49132459a
Hongxu Zhang <hongxu.teoh@gmail.com>
2023-09-22 16:59:27 +0800

    feedback

--- Medical-System-WebApplication/ContactUs.aspx.designer.cs
+++ Medical-System-WebApplication/ContactUs.aspx.designer.cs
@@ -33,57 +33,12 @@ namespace Medical_System_WebApplication
         protected global::System.Web.UI.WebControls.FormView fvFeedback;
 
         /// <summary>
-        /// feedback_name control.
+        /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox feedback_name;
-
-        /// <summary>
-        /// feedback_email control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox feedback_email;
-
-        /// <summary>
-        /// feedback_phone control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox feedback_phone;
-
-        /// <summary>
-        /// feedback_patient control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButtonList feedback_patient;
-
-        /// <summary>
-        /// feedback_textbox control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox feedback_textbox;
-
-        /// <summary>
-        /// feedback_submit control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button feedback_submit;
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
