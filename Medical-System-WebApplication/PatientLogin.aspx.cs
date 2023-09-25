f4c261c679f8540765d0d3b88a7aca655542132d
ions-js04 <behnc.natural@gmail.com>
2023-09-25 12:26:15 +0800

    asdf

--- Medical-System-WebApplication/PatientLogin.aspx.cs
+++ Medical-System-WebApplication/PatientLogin.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -18,42 +19,24 @@ namespace Medical_System_WebApplication
 
         protected void btn_LogIn_Click(object sender, EventArgs e)
         {
-            // check if login email exists in database
+
+            //check if login email exists in database
 
             string sql = "SELECT COUNT(*) FROM Patient WHERE PatientEmailAddress = @PatientEmailAddress";
             string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
             SqlConnection con = new SqlConnection(strCon);
             SqlCommand cmd = new SqlCommand(sql, con);
 
-            string sqlPwd = "SELECT PatientPassword FROM Patient WHERE PatientPassword = @PatientPassword";
-            string strConPwd = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-            SqlConnection conPwd = new SqlConnection(strConPwd);
-            SqlCommand cmdPwd = new SqlCommand(sqlPwd, conPwd);
 
-
-            Session["loggedInEmail"] = loginEmailInput.Text;
             string PatientEmail = loginEmailInput.Text;
-            string PatientPassword = loginPasswordInput.Text;
 
             cmd.Parameters.AddWithValue("@PatientEmailAddress", PatientEmail);
-            cmd.Parameters.AddWithValue("@PatientPassword", PatientPassword);
 
             con.Open();
             int existCount = Convert.ToInt32(cmd.ExecuteScalar()); // run sql statement and return single value
-            string pwd = Convert.ToString(cmdPwd.ExecuteScalar());
-
             if (existCount > 0) // email exists
             {
-                // check if login credentials correct, direct to dashboard
-                if (pwd == PatientPassword)
-                { // if correct password
-                    Response.Redirect("PatientDashboard.aspx");
-                }
-                else
-                {
-                    //ModelState.AddModelError(string.Empty, "Email does not exists.")
-                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Incorrect password. Please try again.')", true);
-                }
+                Response.Redirect("PatientDashboard.aspx");
 
             }
             else
